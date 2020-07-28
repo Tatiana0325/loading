ee8eecdaa13a1670fb6ebbbb33f5e39ea72f8994
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-07-28 20:13:51 +0300

    Second commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,13 @@ import read from './reader';
 import json from './parser';
 
 export default class GameSavingLoader {
-    static load() {
-        async () => {
+    static async load() {
+        try {
             const data = await read();
             const value = await json(data);
             return value;
+        } catch(err) {
+            throw new Error('Ошибка')
         }
     }
 }
